d44e25e25a67881c457eec6609f96979ec433ce9
JoaSaenz <joacotse@gmail.com>
2020-07-24 18:47:15 -0500

    Form based autentication

--- src/main/java/com/gerencia/prextask/security/ApplicationSecurityConfig.java
+++ src/main/java/com/gerencia/prextask/security/ApplicationSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -39,10 +40,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
             .authenticated()
             .and()
             .formLogin()
-            .loginPage("/login").permitAll()
-            .defaultSuccessUrl("/clientes", true)
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/clientes", true)
+                .passwordParameter("password")
+                .usernameParameter("username")
             .and()
-            .rememberMe(); //default 2 weeks
+            .rememberMe() //default 2 weeks
+                .rememberMeParameter("remember-me")
+            .and()
+            .logout()
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me")
+                .logoutSuccessUrl("/login");
     }
 
     @Override
